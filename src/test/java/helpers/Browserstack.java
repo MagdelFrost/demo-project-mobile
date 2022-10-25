b8b0a4d0a9cb5c57f4d63fd81268a43407c3085e
Magdel <savelyzhur98@gmail.com>
2022-10-25 22:17:28 +0600

    Initial commit

---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,27 @@
+package helpers;
+
+import drivers.BrowserstackMobileDriver;
+import io.qameta.allure.restassured.AllureRestAssured;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+import static java.lang.Thread.sleep;
+
+public class Browserstack {
+
+    public static String videoUrl(final String sessionId) {
+
+        String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .filter(new AllureRestAssured())
+                .log().all()
+                .auth().basic(BrowserstackMobileDriver.CONFIG.browserstackUser(),
+                        BrowserstackMobileDriver.CONFIG.browserstackKey())
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().path("automation_session.video_url");
+    }
+}
---
+++ src/test/java/tests/AndroidSearchTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Text;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+
+public class AndroidSearchTests extends TestBase {
+
+    @Test
+    void searchTest() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    void searchBMSTUTest() {
+        step("Type search for BMSTU article", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BMSTU");
+        });
+        step("Verify article found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .findBy(text("Bauman Moscow State Technical University"))
+                        .should(exist));
+    }
+}
